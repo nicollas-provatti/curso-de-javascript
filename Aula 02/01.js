418902bcea2f722078be7b3aa021676f7ba2aea7
nicollas-provatti <npg@ic.ufal.br>
2026-02-25 13:00:37 -0300

    Add files via upload

---
+++ Aula 02/01.js
@@ -0,0 +1,16 @@
+let nome;
+let idade;
+let altura;
+let estudante;
+
+nome = prompt("Qual é seu nome ?");
+idade = prompt("Qual é sua idade ?");
+altura = prompt("Qual é sua altura ?");
+estudante = prompt("Você é estudante ?"); //
+
+alert("Nome: " + nome + "\n" + "Idade: " + idade + "\n" + "Altura: " + altura + "\n" + "Estudante: " + estudante);
+
+console.log("Nome: " + nome); // string
+console.log("Idade: " + idade); // number (inteiro ou real)
+console.log("Altura: " + altura); // number (inteiro ou real)
+console.log("Estudante: " + estudante); // boolean (true ou false)
---
+++ Aula 02/02.js
@@ -0,0 +1,8 @@
+let numero1, numero2;
+
+numero1 = Number(prompt("Digite o primeiro número: "));
+numero2 = Number(prompt("Digite o segundo número:"));
+
+let soma = numero1 + numero2;
+
+console.log("A soma dos número é " + soma);
