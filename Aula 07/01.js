3256fe7a36de7751f7a122cddddb20b577d1f65e
nicollas-provatti <npg@ic.ufal.br>
2026-03-18 11:12:55 -0300

    Add files via upload

---
+++ Aula 07/01.js
@@ -0,0 +1,9 @@
+let numero = 0;
+
+if (numero == 0) {
+  console.log("eh zero!");
+} else if (numero > 0){
+  console.log("eh positivo!");
+} else {
+  console.log("eh negativo!");
+}
---
+++ Aula 07/03.js
@@ -0,0 +1,29 @@
+/* 
+Menu de Hortifruti
+
+1 - Maça
+2 - Pera
+3 - Banana
+4 - Manga
+
+Você escolheu [fruta]
+*/
+
+let opcao = 1;
+
+switch (opcao) {
+  case 1:
+    console.log("Você escolheu a maça!");
+    break;
+  case 2:
+    console.log("Você escolheu a pera!");
+    break;
+  case 3:
+    console.log("Você escolheu a banana!");
+    break;
+  case 4:
+    console.log("Você escolheu a manga!");
+    break;
+  default:
+    console.log("Opção inválida!");
+}
