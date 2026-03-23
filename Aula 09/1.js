27ace6d11a0cf2f6ad4cbdbe8fd76015d3a664f8
nicollas-provatti <npg@ic.ufal.br>
2026-03-23 11:38:47 -0300

    Add files via upload

---
+++ Aula 09/1.js
@@ -0,0 +1,13 @@
+// Quantidade de console's (5)
+// Contador (1 - 5)
+
+// repita esse bloco de código 5 vezes!
+
+let contador = 1; // variável de controle
+// contador <= 5: condição
+// contador++: incremento
+
+while (contador <= 5) {
+  console.log("Olá, mundo! - " + contador);
+  contador++;
+}
---
+++ Aula 09/2.js
@@ -0,0 +1,8 @@
+let numero = 5;
+
+let contador = 1;
+
+while (contador <= 10) {
+  console.log(numero + " + " + contador + " = " + Number(numero + contador));
+  contador++;
+}
